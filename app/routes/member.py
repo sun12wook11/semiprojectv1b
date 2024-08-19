746ebd49d93975ffe9ee1135469c8f117a5084f8
sun12wook11 <sun12wook11@gmail.com>
2024-08-19 11:06:31 +0900

    member router 및 관련 파일 추가

---
+++ app/routes/member.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+member_router = APIRouter()
+templates = Jinja2Templates(directory='templates')
+
+@member_router.get("/join", response_class=HTMLResponse)
+async def join(request: Request):
+    return templates.TemplateResponse("member/join.html", {"request": request})
+
+@member_router.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("member/login.html", {"request": request})
+
+@member_router.get("/myinfo", response_class=HTMLResponse)
+async def myinfo(request: Request):
+    return templates.TemplateResponse("member/myinfo.html", {"request": request})
