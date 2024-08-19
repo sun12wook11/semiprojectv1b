aaa47c9dea3d001f5c315041b91e85e869b16dd5
sun12wook11 <sun12wook11@gmail.com>
2024-08-19 10:11:28 +0900

    인덱스 파일 추가

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="views/templates")
+app.mount("/static", StaticFiles(directory="views/static"), name="static")
 
-
-@app.get("/")
-async def index():
-    return {"message": "Hello World!!"}
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse("index.html", {"request": req})
 
 if __name__ == '__main__':
     import uvicorn
