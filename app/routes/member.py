c793a3da0f88fcc720add8715fb98d92e1fe073f
sun12wook11 <sun12wook11@gmail.com>
2024-08-22 11:19:28 +0900

    세션 생성 여부에 따른 메뉴 표시 처리 로그아웃 기능 구현

--- app/routes/member.py
+++ app/routes/member.py
@@ -12,8 +12,8 @@ member_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 @member_router.get("/join", response_class=HTMLResponse)
-async def join(request: Request):
-    return templates.TemplateResponse("member/join.html", {"request": request})
+async def join(req: Request):
+    return templates.TemplateResponse("member/join.html", {"request": req})
 
 @member_router.post("/join", response_class=HTMLResponse)
 async def joinok(member: NewMember, db: Session = Depends(get_db)):
@@ -36,8 +36,8 @@ async def joinok(member: NewMember, db: Session = Depends(get_db)):
         return RedirectResponse(url='/member/error', status_code=303)
 
 @member_router.get("/login", response_class=HTMLResponse)
-async def login(request: Request):
-    return templates.TemplateResponse("member/login.html", {"request": request})
+async def login(req: Request):
+    return templates.TemplateResponse("member/login.html", {"request": req})
 
 @member_router.post("/login", response_class=HTMLResponse)
 async def loginok(req: Request, db: Session = Depends(get_db)):
@@ -58,13 +58,22 @@ async def loginok(req: Request, db: Session = Depends(get_db)):
         print(f'▶▶▶ loginok 오류: {str(ex)}')
         return RedirectResponse(url='/member/error', status_code=303)
 
+@member_router.get("/logout", response_class=HTMLResponse)
+async def logout(req: Request):
+    req.session.clear() # 생성된 세션 객체 제거
+    return RedirectResponse("/", status_code=303)
+
 @member_router.get("/myinfo", response_class=HTMLResponse)
-async def myinfo(request: Request):
-    return templates.TemplateResponse("member/myinfo.html", {"request": request})
+async def myinfo(req: Request):
+    return templates.TemplateResponse("member/myinfo.html", {"request": req})
 
 @member_router.get("/error", response_class=HTMLResponse)
-async def error(request: Request):
-    return templates.TemplateResponse("member/error.html", {"request": request})
+async def error(req: Request):
+    return templates.TemplateResponse("member/error.html", {"request": req})
+
+@member_router.get("/loginfail", response_class=HTMLResponse)
+async def loginfail(req: Request):
+    return templates.TemplateResponse("member/loginfail.html", {"request": req})
 
 
 
