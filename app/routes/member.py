47a08093bf069397b9b281f700ef34ccdbc430d1
sun12wook11 <sun12wook11@gmail.com>
2024-08-22 10:14:54 +0900

    로그인 처리를 위한 loginok 라우트 작성

--- app/routes/member.py
+++ app/routes/member.py
@@ -39,6 +39,24 @@ async def joinok(member: NewMember, db: Session = Depends(get_db)):
 async def login(request: Request):
     return templates.TemplateResponse("member/login.html", {"request": request})
 
+@member_router.post("/login", response_class=HTMLResponse)
+async def loginok(req: Request, db: Session = Depends(get_db)):
+    # 클라이언트가 보낸 데이터를 request 객체로 받음
+    data = await req.json()
+    try:
+        print('전송한 데이터 : ', data)
+        redirect_url = '/member/loginfail' #로그인 실패시 loginfail 로 이동
+
+
+        if MemberService.login_member():    #로그인 성공시 myinfo로 이동
+            redirect_url = '/member/myinfo'
+
+        return RedirectResponse(url=redirect_url, status_code=303)
+
+    except Exception as ex:
+        print(f'loginok 오류: {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
+
 @member_router.get("/myinfo", response_class=HTMLResponse)
 async def myinfo(request: Request):
     return templates.TemplateResponse("member/myinfo.html", {"request": request})
