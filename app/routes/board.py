9959b4af6e4c14e4d2ce60d97f04e15a0a2e086d
sun12wook11 <sun12wook11@gmail.com>
2024-08-28 12:02:00 +0900

    게시판 로그인 하지않으면 글쓰기 금지

--- app/routes/board.py
+++ app/routes/board.py
@@ -46,6 +46,9 @@ async def list(req: Request, cpg: int, db: Session = Depends(get_db)):
 
 @board_router.get('/write', response_class=HTMLResponse)
 async def write(req: Request):
+    if 'logined_uid' not in req.session: #로그인하지않으면 글쓰기 금지!
+        return RedirectResponse('member/login', 303)
+
     return templates.TemplateResponse('board/write.html', {'request': req})
 
 
