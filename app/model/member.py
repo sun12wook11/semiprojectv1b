0a9c6244a95d05c13e6a73c2d06f5397fed6aa5e
SunsFeed <sun12wook11@gmail.com>
2024-09-25 14:50:21 +0900

    Update member.py

--- app/model/member.py
+++ app/model/member.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import ForeignKey, String  # String 추가
+from sqlalchemy.orm import Mapped, mapped_column
 from app.model.base import Base
 
 class Member(Base):
@@ -7,10 +8,10 @@ class Member(Base):
 
     mno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     userid: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)  # 식별관계
-    passwd: Mapped[str] = mapped_column(String(128), nullable=False)  # 패스워드 길이 지정
-    name: Mapped[str] = mapped_column(String(50), nullable=False)  # 이름 길이 지정
-    email: Mapped[str] = mapped_column(String(100), nullable=False)  # 이메일 길이 지정
-    regdate: Mapped[datetime] = mapped_column(default=datetime.now)  # 등록 날짜
+    passwd: Mapped[str] = mapped_column(String(100))  # 비밀번호 길이 지정
+    name: Mapped[str] = mapped_column(String(50))  # 이름 길이 지정
+    email: Mapped[str] = mapped_column(String(100))  # 이메일 길이 지정
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
 
 
 
