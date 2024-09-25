f40d8af5e7b89cffb05b9ba45104f3c5a2df559b
SunsFeed <sun12wook11@gmail.com>
2024-09-25 14:35:44 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -5,8 +5,7 @@ class Settings(BaseSettings):
     passwd: str = 'ubuntu'
     dbname: str = 'clouds2024'
     dburl: str = 'localhost'
-    #dbconn: str = f'sqlite:///app/{dbname}.db'
-    #?charset=utf8mb4 이모지 추가기능
-    dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
+    dbconn: str = f'sqlite:///app/{dbname}.db'
+    #dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
 
 config = Settings()
